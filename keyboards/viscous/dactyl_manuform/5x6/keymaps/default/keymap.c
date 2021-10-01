3995dd827e4a9d07160e14237ec05d7b5d761cf9
Justin C <justin.cai101@gmail.com>
2021-10-01 01:23:19 -0700

    feat: xo mac layer

--- keyboards/viscous/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/viscous/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -6,8 +6,8 @@
 
 #define _QWERTY 0
 #define _MAC 1
-#define _LOWER 2
-#define _RAISE 3
+#define _RAISE 2
+#define _LOWER 3
 
 
 
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     RAISE, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+     LOWER, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
      KC_MUTE,                 KC_LBRC,KC_RBRC,                                                       KC_BSLS, KC_EQL, KC_MUTE,
-                                      KC_LCTL,KC_ENT,                        KC_SPC, LOWER,
-                                      KC_TAB,_______,                         _______,  KC_DEL,
+                                      KC_LCTL, LSFT_T(KC_ENT),                 KC_SPC, LT(RAISE, KC_BSPC),
+                                      LT(LOWER, KC_DEL), _______,              _______,  KC_DEL,
                                       KC_BSPC, KC_GRV,                        KC_LGUI, KC_LALT
                                                                                     
 
@@ -39,14 +39,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
      KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
      KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,                         KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-     RAISE, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+     LOWER, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                           KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
      KC_MUTE,                 KC_LBRC,KC_RBRC,                                                       KC_BSLS, KC_EQL, KC_MUTE,
-                                      KC_LGUI,KC_ENT,                        KC_SPC, LOWER,
-                                      KC_TAB,_______,                         _______,  KC_DEL,
+                                      KC_LGUI, LSFT_T(KC_ENT),            KC_SPC,  LT(RAISE, KC_BSPC),
+                                      LT(LOWER, KC_DEL), _______,              _______,  KC_DEL,
                                       KC_CAPS, KC_GRV,                        KC_LCTL, KC_LALT
                                                                                   
   ),
-  [_LOWER] = LAYOUT_5x6(
+  [_RAISE] = LAYOUT_5x6(
 
      _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                        _______, _______ , KC_UP , _______ ,_______,_______,
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   ),
 
-  [_RAISE] = LAYOUT_5x6(
+  [_LOWER] = LAYOUT_5x6(
        KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,                        KC_F6  , KC_F7 , KC_F8 , KC_F9 ,KC_F10 ,KC_F11 ,
        _______,_______,_______,_______,_______,_______,                        _______,_______,_______,_______ ,LSFT(KC_LBRC),LSFT(KC_RBRC),
        _______,_______,_______  ,_______,_______,_______,                        _______,_______,_______,_______, KC_LBRC,KC_RBRC,
--- keyboards/viscous/xo/keymaps/default/keymap.c
+++ keyboards/viscous/xo/keymaps/default/keymap.c
@@ -8,35 +8,43 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _DEFAULT 0
-#define _LOWER 1
-#define _RAISE 2
-#define _GAME 3
-#define _MAC 4
+#define _MAC 1
+#define _LOWER 2
+#define _RAISE 3
+#define _GAME 4
 #define _RESET 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* qwerty */
     KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC ,
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS ,
-    KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,             KC_H,  LSFT_T(KC_J), KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
-    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RGUI,   KC_GRV,       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
-    KC_LCTL,  KC_LGUI, KC_LALT, KC_GRV, KC_LCTL, LSFT_T(KC_ENT), KC_LALT,   LT(_RAISE, KC_DEL),   KC_SPC,  LT(_LOWER, KC_BSPC),  KC_MINS, KC_3, KC_ENT,  KC_RGUI
+    KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,               KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RGUI,   KC_GRV,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
+    KC_LCTL,  KC_LGUI, KC_LALT, TO(_MAC), KC_LCTL, LSFT_T(KC_ENT), KC_LALT,   LT(_LOWER, KC_DEL),   KC_SPC,  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
+  ),
+
+    [_MAC] = LAYOUT( /* mac qwerty */
+    KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC ,
+    KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS ,
+    KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,               KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
+    KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RGUI,   KC_GRV,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
+    KC_LCTL,  KC_LGUI, KC_LALT, TO(_DEFAULT), KC_LGUI, LSFT_T(KC_ENT), KC_LALT,   LT(_LOWER, KC_DEL),   KC_SPC,  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
   ),
 
   [_LOWER] = LAYOUT(
-    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
-    KC_SLEP,       KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS,                     KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,               KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS ,
-    TO(_RESET),    KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_MPRV , KC_MPLY , KC_MNXT, KC_TRNS, KC_TRNS ,
-    TO(_GAME),       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
-
-  [_RAISE] = LAYOUT(
     KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_F12,  KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS ,
     TO(_RESET),    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_TRNS
+  ),
+
+  [_RAISE] = LAYOUT(
+    KC_TRNS,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
+    KC_SLEP,       KC_TRNS, KC_TRNS, KC_LPRN, KC_RPRN, KC_TRNS,                     KC_F12,  KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,               KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS ,
+    TO(_RESET),    KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_MPRV , KC_MPLY , KC_MNXT, KC_TRNS, KC_TRNS ,
+    TO(_GAME),       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_GAME] = LAYOUT(
