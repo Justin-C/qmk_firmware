6594c02c3506021beb6b1a692d93e96918c75c10
Justin C <justin.cai101@gmail.com>
2021-11-12 01:56:24 -0800

    feat: xo keyboard updates, game layer

--- keyboards/viscous/xo/keymaps/default/keymap.c
+++ keyboards/viscous/xo/keymaps/default/keymap.c
@@ -9,9 +9,9 @@
 // entirely and just use numbers.
 #define _DEFAULT 0
 #define _MAC 1
-#define _LOWER 2
-#define _RAISE 3
-#define _GAME 4
+#define _GAME 2
+#define _LOWER 3
+#define _RAISE 4
 #define _RESET 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS ,
     KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,               KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RGUI,   KC_GRV,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
-    KC_LCTL,  KC_LGUI, KC_LALT, TO(_MAC), KC_LCTL, LSFT_T(KC_ENT), KC_LALT,   LT(_LOWER, KC_DEL),   KC_SPC,  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
+    KC_LCTL,  KC_LGUI, KC_LALT, TO(_MAC), KC_LCTL, KC_ENT, KC_LALT,   LT(_LOWER, KC_DEL),   LSFT_T(KC_SPC),  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
   ),
 
     [_MAC] = LAYOUT( /* mac qwerty */
@@ -28,7 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS ,
     KC_LSFT,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_MUTE,               KC_H, KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
     KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_RGUI,   KC_GRV,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
-    KC_LCTL,  KC_LGUI, KC_LALT, TO(_DEFAULT), KC_LGUI, LSFT_T(KC_ENT), KC_LALT,   LT(_LOWER, KC_DEL),   KC_SPC,  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
+    KC_LCTL,  KC_LGUI, KC_LALT, TO(_DEFAULT), KC_LGUI, KC_ENT, KC_LALT,   LT(_LOWER, KC_DEL),   LSFT_T(KC_SPC),  LT(_RAISE, KC_BSPC),  KC_BSLS, KC_EQL, KC_BSLS,  KC_RGUI
+  ), 
+
+    [_GAME] = LAYOUT(
+    KC_ESC,  KC_NO  , KC_1  , KC_2  , KC_3  , KC_4  ,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   TO(_DEFAULT)   ,
+    KC_TAB,   KC_TAB  , KC_Q  , KC_W  , KC_E  , KC_R  ,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS ,
+    KC_LSFT,   KC_LSFT,  KC_A,    KC_S,    KC_D, KC_F,   KC_NO,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT ,
+    KC_LSFT,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_SPC,   KC_SPC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_EQL ,
+    KC_LCTL, KC_LCTL,  KC_LALT, KC_LALT, KC_LCTL  , KC_SPC  , KC_SPC  ,  KC_SPC  ,  KC_NO  , LT(_RAISE, KC_BSPC)  , KC_NO  , KC_NO  , KC_NO  , KC_NO
   ),
 
   [_LOWER] = LAYOUT(
@@ -47,20 +55,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_GAME),       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
-  [_GAME] = LAYOUT(
-    KC_ESC,  KC_NO  , KC_1  , KC_2  , KC_3  , KC_4  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , TO(_DEFAULT)   ,
-    KC_TAB,   KC_TAB  , KC_Q  , KC_W  , KC_E  , KC_R  ,                     KC_NO  , KC_NO  , KC_I  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_LSFT,   KC_LSFT,  KC_A,    KC_S,    KC_D, KC_F  ,   KC_NO,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_LSFT,   KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_SPC,   KC_SPC,  KC_NO  , KC_M  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_LCTL, KC_LCTL,  KC_LGUI, KC_LALT, KC_LCTL  , KC_SPC  , KC_SPC  ,  KC_SPC  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO
-  ),
-
 
   [_RESET] = LAYOUT(
     TO(_DEFAULT),  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   KC_C,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
-    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,    KC_A,   KC_B,                   KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,   KC_C,             KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,    KC_A,   KC_B,    KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,  KC_NO  ,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , RESET
   )
 
